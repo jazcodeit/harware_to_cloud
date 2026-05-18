5e7b2be134a5a1b29ba12eba6a251df87a7727b0
Jaz-Ubuntu <jazcodeit@gmail.com>
2026-05-18 11:38:13 -0600

    Update sql schema

--- setup.sql
+++ setup.sql
@@ -2,6 +2,5 @@ CREATE TABLE sensor_data (
     timestamp DATETIME NOT NULL,
     farthest_distance FLOAT NOT NULL,
     closest_distance FLOAT NOT NULL,
-    average_distance FLOAT NOT NULL,
-    PRIMARY KEY (timestamp)
+    average_distance FLOAT NOT NULL
 );
